3b55b2c19197235236c0f67dab1c5826b3a3f417
zxinglong <1098756763@qq.com>
2014-01-02 15:49:31 +0800

    234

---
+++ src/cn/ac/iscas/nfs/service/GetRelatedObjectsResponse.java
@@ -0,0 +1,62 @@
+
+package cn.ac.iscas.nfs.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>getRelatedObjectsResponse complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="getRelatedObjectsResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://service.nfs.iscas.ac.cn/}jsonObject" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getRelatedObjectsResponse", propOrder = {
+    "_return"
+})
+public class GetRelatedObjectsResponse {
+
+    @XmlElement(name = "return")
+    protected JsonObject _return;
+
+    /**
+     * 获取return属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link JsonObject }
+     *     
+     */
+    public JsonObject getReturn() {
+        return _return;
+    }
+
+    /**
+     * 设置return属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JsonObject }
+     *     
+     */
+    public void setReturn(JsonObject value) {
+        this._return = value;
+    }
+
+}
