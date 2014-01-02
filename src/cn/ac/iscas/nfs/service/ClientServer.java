5bb3054eeb13db2ed15fdf7ceeb23062c977ab32
zxinglong <1098756763@qq.com>
2014-01-02 16:08:29 +0800

    23

--- src/cn/ac/iscas/nfs/service/ClientServer.java
+++ src/cn/ac/iscas/nfs/service/ClientServer.java
@@ -10,27 +10,31 @@ public class ClientServer {
 		ServiceImpl factory = new ServiceImpl();
 		ServiceInterface siIns = factory.getServiceImplPort();
 		
-		siIns.createBugReqRel("Bugzilla", "81", "Jira", "NFSPLAT-73");
-		siIns.createBugReqRel("Bugzilla", "81", "Jira", "NFSPLAT-71");
-		siIns.createBugReqRel("Bugzilla", "81", "Jira", "NFSPLAT-72");
-
-		List<String> bugList = new ArrayList<String>();
-		List<String> reqList = new ArrayList<String>();
+//		siIns.createBugReqRel("Bugzilla", "81", "Jira", "NFSPLAT-73");
+//		siIns.createBugReqRel("Bugzilla", "81", "Jira", "NFSPLAT-71");
+//		siIns.createBugReqRel("Bugzilla", "81", "Jira", "NFSPLAT-72");
+//
+//		List<String> bugList = new ArrayList<String>();
+//		List<String> reqList = new ArrayList<String>();
+//		
+//		bugList = siIns.getBugIdByReq("Jira", "NFSPLAT-70", "Bugzilla");
+//		reqList = siIns.getReqIdByBug("Bugzilla", "bug_0", "Jira");
 		
-		bugList = siIns.getBugIdByReq("Jira", "NFSPLAT-70", "Bugzilla");
-		reqList = siIns.getReqIdByBug("Bugzilla", "bug_0", "Jira");
+		String a = siIns.getRelatedObjects("Bugzilla", "bug_0");
+		System.out.println(a);
 		
-		System.out.println("bugList:");
-		for(int i=0; i<bugList.size(); i++)
-		{
-			System.out.print(bugList.get(i)+"\n");
-		}
 		
-		System.out.println("\nreqList:");
-		for(int i=0; i<reqList.size(); i++)
-		{
-			System.out.print(reqList.get(i)+"\n");
-		}
+//		System.out.println("bugList:");
+//		for(int i=0; i<bugList.size(); i++)
+//		{
+//			System.out.print(bugList.get(i)+"\n");
+//		}
+//		
+//		System.out.println("\nreqList:");
+//		for(int i=0; i<reqList.size(); i++)
+//		{
+//			System.out.print(reqList.get(i)+"\n");
+//		}
 	}
 
 }
